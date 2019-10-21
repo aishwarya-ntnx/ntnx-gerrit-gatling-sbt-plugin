7a21f8893c661ea4295e966324df7af82c2d16c7
Aishwarya Srivastava <aishwarya.srivastava@nutanix.com>
2019-10-21 10:57:35 -0700

    Trying out a setup

--- src/test/scala/gerritforge/GerritGitScenario.scala
+++ src/test/scala/gerritforge/GerritGitScenario.scala
@@ -9,11 +9,36 @@ object GerritGitScenario {
 
   implicit val gitConfig = GatlingGitConfiguration()
 
+  // git clone ssh://jenkins.svc@gerrit.eng.nutanix.com:29418/main
   val cloneCommand = new GitRequestBuilder(
     GitRequestSession("clone", s"${testConfig.sshUrl}/${testConfig.project}", "${refSpec}")
   )
 
+  /*
+  // git clone ssh://jenkins.svc@fetch-gerrit.dyn.nutanix.com:29418/main
+  val cloneFromSlaveCommand = new GitRequestBuilder(
+    GitRequestSession("clone", s"${testConfig.slaveSshUrl}/${testConfig.project}", "${refSpec}")
+  )
+
+  // git checkout euphrates-5.11-stable
+  val checkoutCommand = new GitRequestBuilder(
+    GitRequestSession("checkout", s"${testConfig.branch}")
+  )
+
+  // git clone ssh://jenkins.svc@fetch-gerrit.dyn.nutanix.com:29418/toolbox -b master test-toolbox
+  val cloneAndCheckoutCommand = new GitRequestBuilder(
+    GitRequestSession("clone", s"${testConfig.slaveSshUrl}/${testConfig.project}", "-b", "${testConfig.branch}", "${testConfig.dir}")
+  )
+
+  // git rev-parse HEAD
+  val getCurrentRevisionCommand = new GitRequestBuilder(
+    GitRequestSession("rev-parse", "HEAD")
+  )
+
+  // git pull --rebase
+
   val pushCommand = new GitRequestBuilder(
     GitRequestSession("push", s"${testConfig.sshUrl}/${testConfig.project}", "${refSpec}")
   )
+  */
 }
