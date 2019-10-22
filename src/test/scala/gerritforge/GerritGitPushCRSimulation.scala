90928afcb76411976d8f9aaf647cec5940205ac8
Aishwarya Srivastava <aishwarya.srivastava@nutanix.com>
2019-10-22 13:13:44 -0700

    [WIP] Simulation for pushing a code review

--- src/test/scala/gerritforge/GerritGitPushCRSimulation.scala
+++ src/test/scala/gerritforge/GerritGitPushCRSimulation.scala
@@ -7,37 +7,18 @@ import io.gatling.core.scenario.Simulation
 
 import scala.concurrent.duration._
 
-class GerritGitSimulation extends Simulation {
+class GerritGitPushSimulation extends Simulation {
 
   val gitProtocol = GitProtocol()
   val numUsers    = 10
   val feeder = (1 to numUsers) map { idx =>
-
-    Map("refSpec" -> s"master")
+    Map("refSpec" -> s"HEAD:refs/for/master")
   }
-
-  val gitClone = scenario("Git clone from Gerrit")
-    .feed(feeder.circular)
-    .exec(cloneCommand)
-
-  val gitCloneBranch = scenario("Git clone branch from Gerrit")
-    .feed(feeder.circular)
-    .exec(cloneBranchCommand)
-
-  val gitFetch = scenario("Git fetch from Gerrit")
-    .feed(feeder.circular)
-    .exec(cloneCommand)
-    .exec(fetchCommand)
 
   val gitPush = scenario("Git push to Gerrit")
     .feed(feeder.circular)
     .exec(cloneCommand)
     .exec(pushCommand)
-
-  val gitPull = scenario("Git push to Gerrit")
-    .feed(feeder.circular)
-    .exec(cloneCommand)
-    .exec(pullCommand)
 
   setUp(
     gitPush.inject(constantConcurrentUsers(numUsers) during (10 seconds))
